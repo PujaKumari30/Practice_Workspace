5aaae25e8883b6f01156ae161d432baa36a7cd32
kupuja <kupuja@192.168.0.65>
2019-01-05 20:48:45 +0530

    My first ever commit

---
+++ firstApp/src/main/java/com/pkp/practice/firstApp/FirstAppDemo.java
@@ -0,0 +1,28 @@
+package com.pkp.practice.firstApp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class FirstAppDemo {
+	
+	@RequestMapping("/first")
+	@GetMapping
+	public String returnValue() {
+		
+		return "Hello Everyone ! Hey";
+		
+	}
+	
+	
+	@RequestMapping("/hello")	
+	public String returnHello() {
+		return "Hey buddy";
+	}
+
+	
+	
+
+}
